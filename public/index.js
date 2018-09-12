258d53499808196bc68584f7551ef119733189f1
chrisesquerra <esquerra>
2018-09-12 15:48:26 -0500

    Initial commit.  Layout and formatting code written.  No styling or access to JSON completed.

---
+++ public/index.js
@@ -0,0 +1,28 @@
+function writeMovies(movies) {
+
+    for (var i = 0; i < movies.length; i++) {
+        var card = "<div class='card'>";
+        card += "<div class='card-header'>" + movies[i].title + "</div>";
+        card += "<div class='card-body'>";
+        card += "<p class='card image'><!--MOVIE POSTER IMAGES--></p>";
+        card += "<p class='card-text'>\" + movies[i].rating + \"</p>";
+        // WRITE RATINGS AS IMAGES WRITTEN BY CODE IN CORRESPONDING NUMBER
+        card += "<p class='card-footer'>";
+        // for (var j = 0; j < movies[i].tags.length; j++) {
+        //     var cats = "";
+        //     cats = "<button type='button' class='btn btn-info'>" + movies[i].tags[j] + "</button>";
+        //     card += cats;
+        // }
+        card += "</div>";
+        $("#moviesLayout").append(card);
+    }
+
+}
+
+$(document).ready(function(){
+    $("#loadingHeader").text("Loading...");
+    let movies = $.ajax("../db.json");
+    movies.done(writeMovies)
+    }
+});
+
