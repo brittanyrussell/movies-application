0ba35d529e31711a00a5187fd8d6d79a0aebce1e
chrisesquerra <esquerra>
2018-09-13 20:19:53 -0500

    Restored get and post functions.  centered content to prepare for styling pass.  locked headers and footers to allow space for table in center.

--- src/index.js
+++ src/index.js
@@ -1,77 +1,84 @@
+// =================START OF CARD GENERATION FUNCTION==============
+// function writeMovies(movies) {
+//     for (let i = 0; i < movies.length; i++) {
+//         let card = "<div class='card bg-dark'>";
+//         card += "<div class='card-body'>" + "<p>" +movies[i].title+"</p>";
+//         // card += "<p class='card image'>" + movies [i].image + "</p>";
+//         card += "<p class='card-text'>" + movies[i].rating + "</p>";
+//         // WRITE RATINGS AS IMAGES WRITTEN BY CODE IN CORRESPONDING NUMBER
+//         card += "<p class='card-footer'>";
+//         // for (var j = 0; j < movies[i].tags.length; j++) {
+//         //     var cats = "";
+//         //     cats = "<button type='button' class='btn btn-info'>" + movies[i].tags[j] + "</button>";
+//         //     card += cats;
+//         // }
+//         card += "</div>";
+//         $("#moviesLayout").append(card);
+//     }
+// }
 function writeMovies(movies) {
     for (let i = 0; i < movies.length; i++) {
-        let card = "<div class='card'>";
-        card += "<div class='card-header'>" + movies[i].title + "</div>";
-        card += "<div class='card-body'>";
-        card += "<p class='card image'>"  + movies [i].image + "</p>";
-        card += "<p class='card-text'>" + movies[i].rating + "</p>";
-        // WRITE RATINGS AS IMAGES WRITTEN BY CODE IN CORRESPONDING NUMBER
-        card += "<p class='card-footer'>";
-        // for (var j = 0; j < movies[i].tags.length; j++) {
-        //     var cats = "";
-        //     cats = "<button type='button' class='btn btn-info'>" + movies[i].tags[j] + "</button>";
-        //     card += cats;
-        // }
-        card += "</div>";
-        $("#moviesLayout").append(card);
+        let table = "<tr>";
+        table += "<td>" + movies[i].title + "</td>";
+        table += "<td>" + movies[i].rating + "</td>";
+        table+= "</tr>";
+        $("#moviesLayout").append(table);
     }
-
 }
 
+// =================END OF CARD GENERATION FUNCTIONS==============
 
-function userInput() {
+// ====================DOCUMENT LOAD FUNCTIONS====================
+$(document).ready(function () {
+    $(".hide").css("display", "none");
+    $("#loadingHeader").text("Loading...");
 
-    let card = "<div class='card'>";
-    card += "<div class='card-header' id='title'>" + "<input type='text'>" +  "</div>";
-    card += "<div class='card-body'>";
-    card += "<p class='card image' id='image'>" + "<input type='text'>" +   "</p>";
-    card += "<p class='card-text' id='rating'>" +
-       "<select>"+
-        "<option value='1'>1</option>"+
-        "<option value='2'>2</option>"+
-        "<option value='3'>3</option>"+
-        "<option value='4'>4</option>"+
-        "<option value='5'>5</option>"+
+    const {getMovies} = require('./api.js');
+    getMovies().then((movies) => {
+        writeMovies(movies);
 
-    "</select>"
-        + "</p>";
+        $("#loadingHeader").css("display", "none");
+        $("#load").css("display", "none");
+        $(".hide").css("display", "block");
+
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
+    });
+});
+
+// ====================USER INPUT POST REQUEST====================
+
+$("#submit").click(function () {
+    $.ajax({
+        type: "POST",
+        url: "./api/movies",
+        data: {
+            title: $("#title").val(),
+            rating: $("#rating").val(),
+        }
+    });
+    let table = "<table>";
+    table += "<tr><td>" + $("#title").val() + "</td><td></td>";
+    // card += "<p class='card image'>" + movies [i].image + "</p>";
+    table += "<td>" + $("#rating").val() + "</td></tr>";
     // WRITE RATINGS AS IMAGES WRITTEN BY CODE IN CORRESPONDING NUMBER
-    card += "<p class='card-footer'>";
+    table += "<p class='card-footer'>";
     // for (var j = 0; j < movies[i].tags.length; j++) {
     //     var cats = "";
     //     cats = "<button type='button' class='btn btn-info'>" + movies[i].tags[j] + "</button>";
     //     card += cats;
     // }
-    card += "</div>";
-    $("#input").append(card);
-
-}
- // userInput()
-// $.ajax  ( [{
-//     type: "POST",
-//     url: "/db.json",
-//     data:  {    title: $("#title").val(),
-//                 rating: $("#rating").val(),
-//                   id: movies.length+1 }
-//
-// }])
-
-
-
-$(document).ready(function(){
-    $("#loadingHeader").text("Loading...");
-    const {getMovies} = require('./api.js');
-
-    getMovies().then((movies) => {
-        console.log(movies);
-        writeMovies(movies);
-        // userInput()
-
-    }).catch((error) => {
-        alert('Oh no! Something went wrong.\nCheck the console for details.')
-        console.log(error);
-    });
+    table += "</table>";
+    $("#moviesLayout").append(table);
 });
+
+
+
+
+
+
+
 
 
 
